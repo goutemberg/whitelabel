d09966c19b02493c0423e8474fdc2ab6c3bdef46
goutembergpessoa <goutemberg.silva@foxbit.com.br>
2024-11-13 16:40:48 -0300

    ajustes para suber

--- whitelabel/settings.py
+++ whitelabel/settings.py
@@ -74,15 +74,22 @@ WSGI_APPLICATION = 'whitelabel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mgl_base_dados',
+#         'USER': 'usuario_mgl',
+#         'PASSWORD': 'McqAdGu54NougOKPqxHvPjTNVUgYD8XI',  
+#         'HOST': 'dpg-crutsju8ii6s738hnki0-a',
+#         'PORT': '5432',
+#         'CONN_MAX_AGE': 600,  # Conexões persistentes com o banco de dados
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mgl_base_dados',
-        'USER': 'usuario_mgl',
-        'PASSWORD': 'McqAdGu54NougOKPqxHvPjTNVUgYD8XI',  
-        'HOST': 'dpg-crutsju8ii6s738hnki0-a',
-        'PORT': '5432',
-        'CONN_MAX_AGE': 600,  # Conexões persistentes com o banco de dados
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
