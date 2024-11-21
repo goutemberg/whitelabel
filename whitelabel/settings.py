ac42933a80a475a8130ca746da121536a31efa13
goutembergpessoa <goutemberg.silva@foxbit.com.br>
2024-11-21 16:03:49 -0300

    remocao_banco

--- whitelabel/settings.py
+++ whitelabel/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'whitelabel.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
